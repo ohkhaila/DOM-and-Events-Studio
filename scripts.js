20e618380de221b199ee1ffcd00a5905eab3df8e
ohkhaila <khaila.jones@lessannoyingcrm.com>
2020-03-16 18:55:16 -0500

    Steps 1-4 of studio complete. Pausing to engange in present classes exercises and studio

--- scripts.js
+++ scripts.js
@@ -1,2 +1,10 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener('load', function () {
+    alert("page is fully loaded.")
+  })
+
+  //2A
+  
+
+ // object.onclick = function(){myScript};
