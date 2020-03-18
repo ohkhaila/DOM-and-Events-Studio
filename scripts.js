4cb562a697e4a74f2ab114020bc6acd568d2c75a
ohkhaila <khaila.jones@lessannoyingcrm.com>
2020-03-18 14:53:04 -0500

    Moved JS code into the script file. All commands functioning as they should

--- scripts.js
+++ scripts.js
@@ -1,10 +1,56 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
 window.addEventListener('load', function () {
-    alert("page is fully loaded.")
-  })
-
-  //2A
   
+
+//commands
+  const takeoffButton = document.getElementById("takeoff");
+  const landingButton = document.getElementById("landing"); 
+  const abortButton = document.getElementById("missionAbort");
+    
+  //rocket 
+
+  //const rocket = 
+  const flightStatus = document.getElementById("flightStatus");
+  const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+  const shuttleBackground = document.getElementById("shuttleBackground");
+  //direction buttons
+  // const upButton = 
+  // const downButton = 
+  // const rightButton = 
+  // const leftButton = 
+
+
+    takeoffButton.onclick = function(){
+      let confirmReady = window.confirm("Confirm that the shuttle is ready for takeoff.");
+
+      if (confirmReady) {
+          flightStatus.innerHTML = "Shuttle in flight";
+          shuttleBackground.style.backgroundColor = "blue";
+          spaceShuttleHeight.innerHTML = "10000";
+      }
+    }
+
+  //3
+  landingButton.addEventListener('click', function(){
+    let confirmLanding = window.alert("The shuttle is landing. Landing gear engaged.");
+    flightStatus.innerHTML = "The shuttle has landed.";
+    shuttleBackground.style.backgroundColor = "green";
+    spaceShuttleHeight.innerHTML = "0";
+});
+
+  //4
+  abortButton.onclick = function(){
+    let confirmAbort = window.confirm("Confirm that you want to abort the mission.");
+
+    if (confirmAbort) {
+        let changeFlightStatus = document.getElementById("flightStatus").innerHTML = "Mission aborted.";
+        let changeBackgroundColor = document.getElementById("shuttleBackground").style.backgroundColor = "green";
+        let changeHeight = document.getElementById("spaceShuttleHeight").innerHTML = "0";
+    }
+}
+
+
+});  
 
  // object.onclick = function(){myScript};
